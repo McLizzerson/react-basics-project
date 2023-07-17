bdce64c684d75214fc6e29cdd62cd83210507c2e
McLizzerson <liesbeth.fr@gmail.com>
2023-07-17 18:09:30 +0200

    Add Recipecard list to RecipeListPage

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,13 +1,21 @@
-import { Center, Heading } from '@chakra-ui/react';
-import { data } from '../utils/data';
+import { Box, Center, Heading, Stack } from "@chakra-ui/react";
+import { data } from "../utils/data";
+import { RecipeCard } from "../components/RecipeCard";
 
 export const RecipeListPage = () => {
   // You can play around with the console log, but ultimately remove it once you are done
-  console.log(data.hits[0].recipe.label);
+  console.log(data.hits[0]);
 
   return (
-    <Center h="100vh" flexDir="column">
-      <Heading>Your Recipe App</Heading>
-    </Center>
+    <Box height="100vh" flexDir="column">
+      <Heading align="Center" padding={4}>
+        Recipe List Page
+      </Heading>
+      <Stack gap={8}>
+        {data.hits.map((hit) => (
+          <RecipeCard key={hit.recipe.label} recipe={hit} />
+        ))}
+      </Stack>
+    </Box>
   );
 };
