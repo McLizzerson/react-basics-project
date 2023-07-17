e4f829fbd85a7323f0bfa280aad902421e845f7c
McLizzerson <liesbeth.fr@gmail.com>
2023-07-17 16:44:00 +0200

    Add colors to different Tags

--- src/components/ui/Tag.jsx
+++ src/components/ui/Tag.jsx
@@ -1,8 +1,29 @@
 import { Tag } from "@chakra-ui/react";
 
-export const LabelTag = ({ text }) => {
+export const LabelTag = ({ text, typeOfTag }) => {
+  let tagColor;
+
+  console.log(text);
+
+  switch (typeOfTag) {
+    case "mealType":
+    case "dishType":
+      tagColor = "#FFD6A5";
+      break;
+    case "cautions":
+      tagColor = "#FF9B9B";
+      break;
+    case "veganVeg":
+      tagColor = "#CBFFA9";
+      break;
+    case "healthLabels":
+      tagColor = "E1AEFF";
+  }
+
+  console.log(tagColor);
+
   return (
-    <Tag size="sm" variant="solid" colorScheme="teal">
+    <Tag size="sm" variant="solid" color="white" bg={tagColor}>
       {text}
     </Tag>
   );
