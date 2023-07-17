135462fe4acc52dd312b34f4b25cc00ec8c3cf9a
McLizzerson <liesbeth.fr@gmail.com>
2023-07-17 18:10:31 +0200

    Add prop to RecipeCard

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,29 +1,26 @@
-import { Box, Flex, Heading, Image, Text, Stack } from "@chakra-ui/react";
+import { Flex, Heading, Image, Text, Stack } from "@chakra-ui/react";
 import { Card, CardBody } from "@chakra-ui/react";
-import { data } from "../utils/data";
 import { LabelTag } from "./ui/Tag";
 
-export const RecipeCard = () => {
-  const recipeNr = 7;
-  const imageUrl = data.hits[recipeNr].recipe.image;
-  const recipeName = data.hits[recipeNr].recipe.label;
-  const dietLabels = data.hits[recipeNr].recipe.dietLabels;
-  const dishType = data.hits[recipeNr].recipe.dishType;
-  const mealType = data.hits[recipeNr].recipe.mealType;
-  const cautions = data.hits[recipeNr].recipe.cautions;
-  console.log(cautions);
+export const RecipeCard = ({ recipe }) => {
+  const imageUrl = recipe.recipe.image;
+  const recipeName = recipe.recipe.label;
+  const dietLabels = recipe.recipe.dietLabels;
+  const dishType = recipe.recipe.dishType;
+  const mealType = recipe.recipe.mealType;
+  const cautions = recipe.recipe.cautions;
 
   return (
     <Card
       bg="#F9F3DF"
       borderRadius={10}
-      maxWidth="sm"
+      maxWidth="100vw"
       h="35rem"
       // onClick={() => clickFn(item)}
       cursor="pointer"
       _hover={{ transform: "scale(1.05)" }}
     >
-      <Flex w="sm" h="350px">
+      <Flex w="100vw" h="250px">
         <Image
           src={imageUrl}
           alt={recipeName}
