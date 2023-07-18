0a35669524be41500746f733a98cc117fad9ab15
McLizzerson <liesbeth.fr@gmail.com>
2023-07-18 13:12:21 +0200

    Add useState to main App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-import { RecipeListPage } from './pages/RecipeListPage';
+import { Box } from "@chakra-ui/react";
+import { RecipeListPage } from "./pages/RecipeListPage";
+import { RecipePage } from "./pages/RecipePage";
+import { useState } from "react";
 
 export const App = () => {
-  // Your state code here
-  return <RecipeListPage />;
+  const [selectedRecipe, setSelectedRecipe] = useState();
+  console.log(`recipe: ${selectedRecipe}`);
+
+  return (
+    <Box backgroundColor="purple.50" w="100vw">
+      {selectedRecipe ? (
+        <RecipePage recipe={selectedRecipe} clickFn={setSelectedRecipe} />
+      ) : (
+        <RecipeListPage clickFn={setSelectedRecipe} />
+      )}
+    </Box>
+  );
 };
