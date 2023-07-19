1f781b3349b327becdd775d3b85b32a645adefaf
McLizzerson <liesbeth.fr@gmail.com>
2023-07-19 14:14:50 +0200

    Fix RecipeSearch for search on Label

--- src/components/RecipeSearch.jsx
+++ src/components/RecipeSearch.jsx
@@ -5,31 +5,34 @@ import { Flex, Text } from "@chakra-ui/react";
 import { RecipeList } from "./RecipeList";
 
 export const RecipeSearch = ({ clickFn }) => {
-  const [searchField, setSearchField] = useState("paleo");
+  const [searchField, setSearchField] = useState("");
 
   const handleChange = (event) => setSearchField(event.target.value);
 
   const matchedRecipes = data.hits.filter((recipe) => {
-    console.log("this is a recipe");
-    console.log(recipe);
+    // console.log("this is a recipe");
+    // console.log(recipe);
+    console.log(recipe.recipe.label);
 
-    let listOfMatches = [];
     if (recipe.recipe.label.toLowerCase().includes(searchField.toLowerCase())) {
-      listOfMatches.push(recipe);
+      return recipe;
     }
-    return listOfMatches;
   });
 
-  console.log(data);
-  console.log(typeof data.hits);
+  // console.log(data);
+  console.log(`data is a type of: ${typeof data}`);
+  console.log(`matched recipes is length: ${matchedRecipes.length}`);
   console.log(matchedRecipes);
-  console.log(typeof matchedRecipes);
+
+  matchedRecipes.map((recipe) => {
+    console.log(recipe.recipe.label);
+  });
 
   return (
     <Flex direction="column" align="center" gap={4}>
       <TextInput changeFn={handleChange} />
       <Text>{searchField}</Text>
-      {/* <RecipeList recipeList={matchedRecipes} clickFn={clickFn} /> */}
+      <RecipeList recipeList={matchedRecipes} clickFn={clickFn} />
     </Flex>
   );
 };
