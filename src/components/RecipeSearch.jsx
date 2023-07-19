41beddc0c5020330cdfdc0362178d209ca13b06e
McLizzerson <liesbeth.fr@gmail.com>
2023-07-19 13:40:51 +0200

    Try to implement searchfunction (end of 18/7)

--- src/components/RecipeSearch.jsx
+++ src/components/RecipeSearch.jsx
@@ -1,23 +1,35 @@
 import { useState } from "react";
 import { data } from "../utils/data";
 import { TextInput } from "./ui/TextInput";
-import { Center, Flex, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
+import { RecipeList } from "./RecipeList";
 
 export const RecipeSearch = ({ clickFn }) => {
-  const [searchField, setSearchField] = useState("");
+  const [searchField, setSearchField] = useState("paleo");
 
   const handleChange = (event) => setSearchField(event.target.value);
 
   const matchedRecipes = data.hits.filter((recipe) => {
-    return recipe.recipe.label
-      .toLowerCase()
-      .includes(searchField.toLowerCase());
+    console.log("this is a recipe");
+    console.log(recipe);
+
+    let listOfMatches = [];
+    if (recipe.recipe.label.toLowerCase().includes(searchField.toLowerCase())) {
+      listOfMatches.push(recipe);
+    }
+    return listOfMatches;
   });
+
+  console.log(data);
+  console.log(typeof data.hits);
+  console.log(matchedRecipes);
+  console.log(typeof matchedRecipes);
 
   return (
     <Flex direction="column" align="center" gap={4}>
       <TextInput changeFn={handleChange} />
       <Text>{searchField}</Text>
+      {/* <RecipeList recipeList={matchedRecipes} clickFn={clickFn} /> */}
     </Flex>
   );
 };
