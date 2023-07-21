253df8f9a837eb8ac6cb1aef129ba6d24ba13d08
McLizzerson <liesbeth.fr@gmail.com>
2023-07-21 15:13:55 +0200

    Change styling

--- src/components/ui/TextInput.jsx
+++ src/components/ui/TextInput.jsx
@@ -1,16 +1,16 @@
 import { Input } from "@chakra-ui/react";
 
-export const TextInput = ({ changeFn, ...props }) => {
+export const TextInput = ({ changeFn }) => {
   return (
     <Input
       textAlign="center"
-      placeholder="Recipe name or health label"
+      placeholder="🔎 Recipe name or health label"
       variant="unstyled"
-      bg="white"
-      width="100%"
+      bg="#F9FAF2"
+      width="50%"
+      minWidth="250px"
       padding={2}
       onChange={changeFn}
-      {...props}
     ></Input>
   );
 };
