eabecf633d68ffa50be1f5767763a3be7713755a
McLizzerson <liesbeth.fr@gmail.com>
2023-07-21 15:17:38 +0200

    Delete excess code from RecipeCard

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Image, Text, Stack } from "@chakra-ui/react";
+import { Flex, Heading, Image, Text } from "@chakra-ui/react";
 import { Card, CardBody } from "@chakra-ui/react";
 import { LabelTag } from "./ui/Tag";
 
@@ -10,7 +10,7 @@ export const RecipeCard = ({ recipe, clickFn }) => {
   const mealType = recipe.recipe.mealType;
   const cautions = recipe.recipe.cautions;
 
-  // CHECK FOR VEGAN AND OR VEGETARIAN
+  // Check if recipe is vegetarian or vegan
   let vegetarian = false;
   let vegan = false;
   if (recipe.recipe.healthLabels.includes("Vegetarian")) {
@@ -20,7 +20,7 @@ export const RecipeCard = ({ recipe, clickFn }) => {
     vegan = true;
   }
 
-  // CHECK IF THERE ARE CAUTIONS
+  // Check if the recipe has any cautions
   let caution = false;
   if (cautions.length > 0) {
     caution = true;
@@ -28,59 +28,64 @@ export const RecipeCard = ({ recipe, clickFn }) => {
 
   return (
     <Card
-      bg="#F9F3DF"
-      borderRadius={10}
-      maxWidth="100vw"
-      h="35rem"
+      bg="#F9FAF2"
+      maxWidth="2xs"
+      h="auto"
       onClick={() => clickFn(recipe)}
       cursor="pointer"
       _hover={{ transform: "scale(1.05)" }}
     >
-      <Flex w="100vw" h="250px">
-        <Image
-          src={imageUrl}
-          alt={recipeName}
-          objectFit="cover"
-          borderTopRadius={10}
-          overflow="hidden"
-        />
+      {/* Recipe image */}
+      <Flex w="2xs" h="250px">
+        <Image src={imageUrl} alt={recipeName} objectFit="cover" />
       </Flex>
 
-      <CardBody height="50%">
-        <Heading size="md" align="center">
+      {/* Start of the body of the card */}
+      <CardBody>
+        <Heading size="md" color="#475223" align="center" w="100%" h="75px">
           {recipeName}
         </Heading>
 
-        <Stack mt="4" spacing="2">
-          <Flex justify="center" gap={4} paddingBottom={4}>
+        {/* Container for 3 flexboxes with meal & dish-, diet- and veg/vegan-labels  */}
+        <Flex direction="column" gap={4} h="150px">
+          <Flex justify="center" gap={4} wrap="wrap">
             <LabelTag text={mealType} typeOfTag="mealType" />
             <LabelTag text={dishType} typeOfTag="dishType" />
           </Flex>
 
-          <Flex justify="center" gap={4} paddingBottom={4}>
+          <Flex justify="center" gap={4} wrap="wrap">
             {dietLabels.map((item) => (
               <LabelTag key={item} text={item} typeOfTag="dietLabel" />
             ))}
           </Flex>
 
-          <Flex justify="center" gap={4} paddingBottom={4}>
+          <Flex justify="center" gap={4}>
             {vegetarian && (
               <LabelTag text="Vegetarian" typeOfTag="healthLabel" />
             )}
             {vegan && <LabelTag text="Vegan" typeOfTag="healthLabel" />}
           </Flex>
+        </Flex>
 
-          {caution && (
+        {/* Check for cautions, if so make tags */}
+        {caution ? (
+          <>
             <Text align="center">
               <b>Caution</b>
             </Text>
-          )}
-          <Flex justify="center" gap={4} paddingBottom={4}>
-            {cautions.map((item) => (
-              <LabelTag key={item} text={item} typeOfTag="cautions" />
-            ))}
-          </Flex>
-        </Stack>
+            <Flex justify="center" gap={4} wrap="wrap">
+              {cautions.map((item) => (
+                <LabelTag key={item} text={item} typeOfTag="cautions" />
+              ))}
+            </Flex>
+          </>
+        ) : (
+          <Text align="center">
+            <b>Caution</b>
+            <br />
+            <LabelTag text={"None"} typeOfTag="none" />
+          </Text>
+        )}
       </CardBody>
     </Card>
   );
