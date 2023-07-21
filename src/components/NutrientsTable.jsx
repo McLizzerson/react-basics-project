231ee63f655c0bae3793988c62012c91fb62b730
McLizzerson <liesbeth.fr@gmail.com>
2023-07-21 18:20:01 +0200

    Clean up NutrientTable

--- src/components/NutrientsTable.jsx
+++ src/components/NutrientsTable.jsx
@@ -1,47 +1,35 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 
 export const NutrientTable = ({ list }) => {
-  const nutrients = ["ENERC_KCAL", "FAT", "CHOCDF", "PROCNT", "CHOLE", "NA"];
+  const arrayList = Object.entries(list);
 
-  const energy = list.ENERC_KCAL.quantity.toFixed(2);
-  const energyUnit = list.ENERC_KCAL.unit;
-  const fat = list.FAT.quantity.toFixed(2);
-  const fatUnit = list.FAT.unit;
-  const carbs = list.CHOCDF.quantity.toFixed(2);
-  const carbsUnit = list.CHOCDF.unit;
-  const protein = list.PROCNT.quantity.toFixed(2);
-  const proteinUnit = list.PROCNT.unit;
-  const cholesterol = list.CHOLE.quantity.toFixed(2);
-  const cholesterolUnit = list.CHOLE.unit;
-  const sodium = list.NA.quantity.toFixed(2);
-  const sodiumUnit = list.NA.unit;
+  const nutrientList = arrayList.filter((nutrient) => {
+    switch (nutrient[0]) {
+      case "ENERC_KCAL":
+      case "FAT":
+      case "CHOCDF":
+      case "PROCNT":
+      case "CHOLE":
+      case "NA":
+        return nutrient;
+    }
+  });
 
   return (
-    <Grid templateColumns="repeat(2, 1fr)" align="left" columnGap={4}>
-      <GridItem>Total Energy:</GridItem>
-      <GridItem>
-        {energy} {energyUnit}
-      </GridItem>
-      <GridItem>Fat:</GridItem>
-      <GridItem>
-        {fat} {fatUnit}
-      </GridItem>
-      <GridItem>Carbs:</GridItem>
-      <GridItem>
-        {carbs} {carbsUnit}
-      </GridItem>
-      <GridItem>Protein:</GridItem>
-      <GridItem>
-        {protein} {proteinUnit}
-      </GridItem>
-      <GridItem>Cholesterol:</GridItem>
-      <GridItem>
-        {cholesterol} {cholesterolUnit}
-      </GridItem>
-      <GridItem>Sodium:</GridItem>
-      <GridItem>
-        {sodium} {sodiumUnit}
-      </GridItem>
+    <Grid templateColumns="repeat(2, 1fr)" align="left" columnGap={8}>
+      {nutrientList.map((nutrient) => {
+        const name = nutrient[1].label;
+        const quantity = nutrient[1].quantity.toFixed(2);
+        const unit = nutrient[1].unit;
+        return (
+          <>
+            <GridItem key={name}>{name}: </GridItem>
+            <GridItem>
+              {quantity} {unit}
+            </GridItem>
+          </>
+        );
+      })}
     </Grid>
   );
 };
