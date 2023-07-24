97ec54995c7605183d37df04c3a52cad24cbddf3
McLizzerson <liesbeth.fr@gmail.com>
2023-07-24 13:06:31 +0200

    Change font

--- src/components/ui/theme.jsx
+++ src/components/ui/theme.jsx
@@ -2,7 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
   fonts: {
-    heading: `'frutiger', sans-serif`,
-    body: `'frutiger', sans-serif`,
+    heading: `'Arial', sans-serif`,
+    body: `'Arial', sans-serif`,
   },
 });
