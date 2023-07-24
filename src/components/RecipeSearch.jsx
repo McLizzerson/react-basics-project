2d42e148665ab9a09fdd7c858ca374cef5f5c2a4
McLizzerson <liesbeth.fr@gmail.com>
2023-07-24 12:56:03 +0200

    Change checkbox to radiogroup

--- src/components/RecipeSearch.jsx
+++ src/components/RecipeSearch.jsx
@@ -1,51 +1,36 @@
 import { useState } from "react";
 import { data } from "../utils/data";
 import { TextInput } from "./ui/TextInput";
-import { Flex, Checkbox, Box } from "@chakra-ui/react";
+import { Flex, Stack } from "@chakra-ui/react";
 import { RecipeList } from "./RecipeList";
+import { Radio, RadioGroup } from "@chakra-ui/react";
 
 export const RecipeSearch = ({ clickFn }) => {
   // Searchfield
   const [searchField, setSearchField] = useState("");
   const handleChange = (event) => setSearchField(event.target.value);
 
-  // Checkbox Vegetarian
-  const [checkedVeg, setCheckedVeg] = useState(false);
-  const handleCheckboxChangeVeg = () => {
-    setCheckedVeg(!checkedVeg);
-  };
-
-  // Checkbox VEGAN
-  const [checkedVegan, setCheckedVegan] = useState(false);
-  const handleCheckboxChangeVegan = () => {
-    setCheckedVegan(!checkedVegan);
-  };
-
-  // Checkbox PESCETARIAN
-  const [checkedPesc, setCheckedPesc] = useState(false);
-  const handleCheckboxChangePesc = () => {
-    setCheckedPesc(!checkedPesc);
-  };
+  const [value, setValue] = useState("all");
 
   let dataMassaged;
 
-  switch (true) {
-    case checkedVeg:
+  switch (value) {
+    case "veg":
       dataMassaged = data.hits.filter((recipe) => {
         return recipe.recipe.healthLabels.includes("Vegetarian");
       });
       break;
-    case checkedVegan:
+    case "vegan":
       dataMassaged = data.hits.filter((recipe) => {
         return recipe.recipe.healthLabels.includes("Vegan");
       });
       break;
-    case checkedPesc:
+    case "pesc":
       dataMassaged = data.hits.filter((recipe) => {
         return recipe.recipe.healthLabels.includes("Pescatarian");
       });
       break;
-    default:
+    case "all":
       dataMassaged = data.hits;
   }
 
@@ -63,11 +48,14 @@ export const RecipeSearch = ({ clickFn }) => {
   return (
     // returns a container with the checkbox, searchfield and the resulting list or recipes
     <Flex direction="column" align="center" gap={4} w="100%">
-      <Flex gap={4} justify="center" bg="white" w="100%" padding={2}>
-        <Checkbox onChange={handleCheckboxChangeVeg}>Vegetarian</Checkbox>
-        <Checkbox onChange={handleCheckboxChangeVegan}>Vegan</Checkbox>
-        <Checkbox onChange={handleCheckboxChangePesc}>Pescetarian</Checkbox>
-      </Flex>
+      <RadioGroup onChange={setValue} value={value} colorScheme="green">
+        <Stack direction="row">
+          <Radio value="all">All</Radio>
+          <Radio value="veg">Vegetarian</Radio>
+          <Radio value="vegan">Vegan</Radio>
+          <Radio value="pesc">Pescatarian</Radio>
+        </Stack>
+      </RadioGroup>
 
       <TextInput changeFn={handleChange} />
 
